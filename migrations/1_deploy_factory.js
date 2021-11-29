076387561d48dbf1c6784aa621b25ff223c1f73c
johni1228 <akirus1228@gmail.com>
2021-11-30 03:28:26 +0900

    chore(testnet): Update migration

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -2,9 +2,5 @@ const BorealisswapFactory = artifacts.require("BorealisswapFactory");
 
 module.exports = function (deployer, network, accounts) {
   let currentAccount = accounts[0]
-  if(network == 'testnet') {
-    console.warn('WARNING: Using account[1] for testnet')
-    currentAccount = accounts[0]
-  }
   deployer.deploy(BorealisswapFactory, currentAccount, {from: currentAccount});
 };
