bf42355e7ab249b013d1523b57a432ebb95b687d
johni1228 <akirus1228@gmail.com>
2021-11-30 05:20:27 +0900

    chore: Delete unused col in 1_deploy_factory.js

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -1,7 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const BorealisswapFactory = artifacts.require("BorealisswapFactory");
 
 module.exports = function (deployer) {
-
   deployer.deploy(Migrations);
 };
