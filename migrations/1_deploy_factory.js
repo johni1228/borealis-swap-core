066750f2b862d736b92c754c6c2fd41e0ce65eda
johni1228 <akirus1228@gmail.com>
2021-11-30 05:12:35 +0900

    feat: Add 2_deploy_core.js

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -1,6 +1,7 @@
+const Migrations = artifacts.require("Migrations");
 const BorealisswapFactory = artifacts.require("BorealisswapFactory");
 
-module.exports = function (deployer, network, accounts) {
-  let currentAccount = accounts[0]
-  deployer.deploy(BorealisswapFactory, currentAccount, {from: currentAccount});
+module.exports = function (deployer) {
+
+  deployer.deploy(Migrations);
 };
---
+++ migrations/2_deploy_core.js
@@ -0,0 +1,6 @@
+const BorealisswapFactory = artifacts.require("BorealisswapFactory");
+const _feeToSetter = '0x8A1eAA7f43D44D06ac1b7677FD6B979538EBc652';
+
+module.exports = function (deployer) {
+  deployer.deploy(BorealisswapFactory,_feeToSetter);
+};
