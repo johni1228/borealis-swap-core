ace846e9a275fa35a4ab602b807378a6c82ca8c1
johni1228 <akirus1228@gmail.com>
2021-11-30 17:04:54 +0900

    chore: Update gasPrice

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -1,7 +1,6 @@
 const BorealisswapFactory = artifacts.require("BorealisswapFactory");
 const _feeToSetter = '0x8A1eAA7f43D44D06ac1b7677FD6B979538EBc652';
 
-module.exports = function (deployer, accounts) {
-  let currentAccount = accounts[0]
-  deployer.deploy(BorealisswapFactory, currentAccount, {from: currentAccount});
+module.exports = function (deployer) {
+  deployer.deploy(BorealisswapFactory, _feeToSetter);
 };
