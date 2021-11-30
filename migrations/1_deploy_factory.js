cbdd36371626b2260a002e8667943d30bd42fecf
johni1228 <akirus1228@gmail.com>
2021-11-30 16:12:13 +0900

    chore: update migrations scripts

--- migrations/1_deploy_factory.js
+++ migrations/1_deploy_factory.js
@@ -1,5 +1,6 @@
-const Migrations = artifacts.require("Migrations");
+const BorealisswapFactory = artifacts.require("BorealisswapFactory");
+const _feeToSetter = '0x8A1eAA7f43D44D06ac1b7677FD6B979538EBc652';
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(BorealisswapFactory,_feeToSetter);
 };
